b0bfba36ee7f16eea02ad613001ea7d1af417be5
Nathan Gill <nrgill28@gmail.com>
2020-11-15 15:41:09 -0500

    Initial commit

---
+++ DeliExampleMod/src/DeliExampleMod.cs
@@ -0,0 +1,21 @@
+﻿using Deli;
+
+namespace DeliExampleMod
+{
+    // Deli Mods are just mono behaviours that get added to a global game object when the game first starts.
+    // Generally you want to put your setup code in Awake()
+    public class DeliExampleMod : DeliMod
+    {
+        private void Awake()
+        {
+            // Hello, world!
+            BaseMod.Log.LogInfo("Hello, world!");
+
+            // Fetch and log the example resource
+            var textResource = BaseMod.Resources.Get<string>("Resources/ExampleTextResource.txt").Unwrap();
+            BaseMod.Log.LogInfo("Text resource contents: " + textResource);
+            
+            // TODO: Put the rest of your setup code here
+        }
+    }
+}
---
+++ DeliExampleMod/src/ExampleAssetReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Deli;
+using UnityEngine;
+using Valve.Newtonsoft.Json;
+
+namespace DeliExampleMod
+{
+    // Asset readers are classes which allow you to easily read a resource from a mod as a certain type.
+    // In this case, we make an asset reader for our ExampleAsset
+    [QuickUnnamedBind]
+    public class ExampleAssetReader : IAssetReader<ExampleJsonAsset>
+    {
+        public ExampleJsonAsset ReadAsset(byte[] raw)
+        {
+            // Decode the string from the raw bytes
+            var str = Encoding.UTF8.GetString(raw);
+            
+            // Return our parsed asset.
+            return new ExampleJsonAsset
+            {
+                Message = str
+            };
+        }
+    }
+}
